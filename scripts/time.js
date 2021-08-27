41cda972678152091b39bfc490b39c0829b75c81
Qubisted <78350166+Qubisted@users.noreply.github.com>
2021-08-27 09:33:29 +0530

    Changes to time API

--- scripts/time.js
+++ scripts/time.js
@@ -1,17 +1,63 @@
+
 function getTime(){
     var today = new Date();
-    var time24 = today.getHours() + ":" + today.getMinutes() +  today.getSeconds();
-    var timeString = time24;
+    var time = today.getHours() + ":" + today.getMinutes() + ":" +   today.getSeconds();
+    var timeString = time;
     var H = +timeString.substr(0, 2);
     var h = H % 12 || 12;
     var ampm = (H < 12 || H === 24) ? " AM" : " PM";
-    timeString = h + timeString.substr(2, 3) + ampm;
+    timeString = h + timeString.substr(1, 3) + ampm;
+    console.log(timeString);
     document.getElementById("currenttime").value = timeString;
 };
+
+
 
 function saveInput(){
     var pagenotfound = document.getElementById("ay").innerHTML  = "404 could not fetch the results.."
 }
 
 
-getTime();
+// api url
+const api_url =
+	"https://employeedetails.free.beeceptor.com/my/api/path";
+
+// Defining async function
+async function getapi(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to hide the loader
+
+// Function to define innerHTML for HTML table
+function show(data) {
+	let tab =
+		`<tr>
+		<th>Name</th>
+		</tr>`;
+	
+	// Loop to access all rows
+	for (let r of data) {
+		tab += `<tr>
+	<td>${r.name} </td>	
+</tr>`;
+	}
+	// Setting innerHTML as tab variable
+	document.getElementById("currenttime").innerHTML = tab;
+}
+
+
+show();
+
